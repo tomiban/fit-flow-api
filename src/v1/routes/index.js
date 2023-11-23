e96a21f56f32220184ce3ec989f17bc2adfc0316
tomiban <81537786+tomiban@users.noreply.github.com>
2023-11-23 19:56:27 -0300

    fix merge errors

--- src/v1/routes/index.js
+++ src/v1/routes/index.js
@@ -1,11 +1,9 @@
 import express from "express";
-import routineRoutes from "./routinesRoutes.js";
 import usersRoutes from "./usersRoutes.js";
 import exercisesRoutes from "./exercisesRoutes.js";
 
 const router = express.Router();
 
-router.use("/routines", routineRoutes);
 router.use("/users", usersRoutes);
 // router.use("/categories", usersRoutes);
 router.use("/exercises", exercisesRoutes);
