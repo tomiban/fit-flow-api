e9c7e6b4c85c0fbc852e156d0f6f5f2de03b0f6c
BravoArturo <aarturoo97@gmail.com>
2023-11-23 19:30:30 -0300

    commit before merge

--- src/services/routineService.js
+++ src/services/routineService.js
@@ -1,36 +1,34 @@
-
 import { v4 as uuid } from "uuid";
 
 const getAllRoutines = () => {
-	const allRoutines = Rutinas.getAllRoutinesDb();
-	return allRoutines;
+  const allRoutines = Rutinas.getAllRoutinesDb();
+  return allRoutines;
 };
 const getRoutineById = (routineId) => {
-	const routine = Rutinas.getRoutineById(routineId);
-	return routine;
+  const routine = Rutinas.getRoutineById(routineId);
+  return routine;
 };
 const createNewRoutine = (routine) => {
-	const newRoutine = {
-		...routine,
-	};
+  const newRoutine = {
+    ...routine,
+  };
 
-	const createdRoutine = Rutinas.createNewRoutine(newRoutine);
-	return createdRoutine;
+  const createdRoutine = Rutinas.createNewRoutine(newRoutine);
+  return createdRoutine;
 };
 
 const updateRoutine = (routineId, changes) => {
-	const updatedRoutine = Rutinas.updateRoutine(routineId, changes);
-	return updatedRoutine;
+  const updatedRoutine = Rutinas.updateRoutine(routineId, changes);
+  return updatedRoutine;
 };
 const deleteRoutine = (routineId) => {
-	
-	return Rutinas.deleteRoutine(routineId);
+  return Rutinas.deleteRoutine(routineId);
 };
 
 export default {
-	getAllRoutines,
-	getRoutineById,
-	createNewRoutine,
-	updateRoutine,
-	deleteRoutine,
+  getAllRoutines,
+  getRoutineById,
+  createNewRoutine,
+  updateRoutine,
+  deleteRoutine,
 };
