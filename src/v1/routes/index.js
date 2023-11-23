33b1fbd056c509ce1edaa180e90407722d3fae5d
tomiban <81537786+tomiban@users.noreply.github.com>
2023-11-23 15:49:24 -0300

    Practice files that were not part of the app's code have been removed.

--- src/v1/routes/index.js
+++ src/v1/routes/index.js
@@ -1,9 +1,8 @@
 import express from 'express';
-import routineRoutes from './routinesRoutes.js';
 import usersRoutes from './usersRoutes.js'
 const router = express.Router();
 
-router.use('/routines',routineRoutes);
+
 router.use('/users', usersRoutes)
 
 
