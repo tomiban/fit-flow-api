4323438d5f9d8d757a49a4946271b1ac39506f0a
tomiban <81537786+tomiban@users.noreply.github.com>
2023-12-01 09:54:12 -0300

    The routes, model, and controllers for categories are created.

--- src/utils/handleValidator.js
+++ src/utils/handleValidator.js
@@ -9,3 +9,8 @@ export const validateResults = (req, res, next) => {
         res.send({errors: error.array()});
     }
 };
+
+export const isFieldAlreadyInUse = async (fieldName, value, userId, model) => {
+    const existingRecord = await model.findOne({[fieldName]: value});
+    return existingRecord && existingRecord._id.toString() !== userId;
+};
