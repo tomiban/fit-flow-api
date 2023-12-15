6bf50aa2ed36471218eb89ba6c231156b48fcd2c
tomiban <81537786+tomiban@users.noreply.github.com>
2023-12-15 11:24:08 -0300

    zod validation are finished

--- src/middlewares/sanitize.js
+++ src/middlewares/sanitize.js
@@ -1,35 +1,5 @@
 import {body, validationResult} from "express-validator";
 
-/* export const validatorRegisterUser = [
-    body("username").exists().trim().escape(),
-
-    check("firstName")
-        .exists()
-        .notEmpty()
-        .isLength({min: 2, max: 40})
-        .withMessage("First name must be between 2 and 40 characters"),
-
-    check("lastName")
-        .exists()
-        .notEmpty()
-        .isLength({min: 2, max: 40})
-        .withMessage("Last name must be between 2 and 40 characters"),
-
-    check("age").exists().notEmpty().isInt({min: 5, max: 150}).withMessage("Age must be a number between 5 and 150"),
-
-    check("email").exists().notEmpty().isEmail().withMessage("You must provide a valid email"),
-
-    check("password")
-        .exists()
-        .notEmpty()
-        .isLength({min: 6, max: 50})
-        .withMessage("Password must be at least 6 characters"),
-
-    check("birthDate").optional().toDate(),
-
-    (req, res, next) => validateResults(req, res, next),
-]; */
-
 const sanitizeLogin = [
     body("username").trim().escape(),
     body("password").trim().escape(),
